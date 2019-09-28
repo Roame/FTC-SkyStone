9e2406503fa2d94def4e8c96a5a6798c6e06d5a0
Roame Hildebrand <roamehildebrand@gmail.com>
2019-09-28 07:56:56 -0400

    Reworking project structure within git

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Main Teleop", group = "Competition")
+public class Main extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
