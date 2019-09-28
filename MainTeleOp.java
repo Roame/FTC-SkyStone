ac3d0dc0b16f382bf6651767c28f21a708f5a3f8
Roame Hildebrand <roamehildebrand@gmail.com>
2019-09-28 08:15:39 -0400

    Cleaned up current files

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
+@TeleOp(name = "Main TeleOp", group = "Competition")
 public class MainTeleOp extends OpMode {
-    //Test checkout
     @Override
     public void init() {
 
