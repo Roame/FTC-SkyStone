b87411c0681fc4b900d97622babce2744c30a4c9
STEAM Powered <steampowered12873@gmail.com>
2019-09-29 16:16:49 -0400

    Created a stone intake class

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -4,15 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrivetrain;
+import org.firstinspires.ftc.teamcode.Subsystems.StoneIntake;
 
 @TeleOp(name = "Main TeleOp", group = "Competition")
 public class MainTeleOp extends OpMode {
     Constants constants = new Constants();
     MecanumDrivetrain mecDrive = new MecanumDrivetrain();
+    StoneIntake stoneIntake = new StoneIntake(hardwareMap);
 
     @Override
     public void init() {
         mecDrive.initDrive(hardwareMap);
+        stoneIntake.initStoneIntake();
     }
   
     @Override
