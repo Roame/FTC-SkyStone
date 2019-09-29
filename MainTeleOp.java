54067613febb5516387142fbdc6fd2574db7492d
STEAM Powered <steampowered12873@gmail.com>
2019-09-29 15:04:10 -0400

    Introduced Mecanum Drive

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -5,13 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Main TeleOp", group = "Competition")
 public class MainTeleOp extends OpMode {
+    MecanumDrive MecD = new MecanumDrive();
+
     @Override
     public void init() {
-
+        MecD.InitDrive(hardwareMap);
     }
 
     @Override
     public void loop() {
-
+        MecD.Drive(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y);
     }
 }
--- MecanumDrive.java
+++ MecanumDrive.java
@@ -1,4 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class MecanumDrive {
+    private DcMotor FR, FL, BR, BL;
+
+
+
+
+    public MecanumDrive(){
+
+    }
+    public void InitDrive(HardwareMap HW){
+        FR = HW.get(DcMotor.class, "FR");
+        FL = HW.get(DcMotor.class, "FL");
+        BR = HW.get(DcMotor.class, "BR");
+        BL = HW.get(DcMotor.class, "BL");
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.FORWARD);
+        BR.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+    public void Drive(float LeftJoyStickX, float RightJoyStickX, float RightJoyStickY){
+
+        FR.setPower(Clip(-1, 1, RightJoyStickY+RightJoyStickX-LeftJoyStickX));
+        FL.setPower(Clip(-1, 1, RightJoyStickY+RightJoyStickX+LeftJoyStickX));
+        BR.setPower(Clip(-1, 1, RightJoyStickY-RightJoyStickX-LeftJoyStickX));
+        BL.setPower(Clip(-1, 1, RightJoyStickY-RightJoyStickX+LeftJoyStickX));
+
+
+
+    }
+    private double Clip(double Min, double Max, double Value){
+        double Cliped;
+        if (Value>Max) {
+            Cliped=Max;
+        } else if (Value<Min){
+            Cliped=Min;
+        } else {
+            Cliped=Value;
+        }
+        return Cliped;
+
+
+
+
+
+    }
+
+
 }
