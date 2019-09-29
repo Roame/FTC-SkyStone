6614b1d47e220738aa3e645e4d65c74a7e722fa4
STEAM Powered <steampowered12873@gmail.com>
2019-09-29 15:01:13 -0400

    Introduced a constants class

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Const;
+
 @TeleOp(name = "Main TeleOp", group = "Competition")
 public class MainTeleOp extends OpMode {
+    Constants constants = new Constants();
+
     @Override
     public void init() {
-
     }
 
     @Override
