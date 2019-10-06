9d84db1d12cda3dcd8d3e6301ec82a24d01f0d58
STEAM Powered <steampowered12873@gmail.com>
2019-10-06 16:36:13 -0400

    Initial Commit

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -1,32 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.transition.Slide;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrivetrain;
+import org.firstinspires.ftc.teamcode.Subsystems.SlideDrivetrain;
 import org.firstinspires.ftc.teamcode.Subsystems.StoneIntake;
 
 @TeleOp(name = "Main TeleOp", group = "Competition")
 public class MainTeleOp extends OpMode {
-    MecanumDrivetrain mecDrive = new MecanumDrivetrain();
+    SlideDrivetrain slideDrive = new SlideDrivetrain();
     StoneIntake stoneIntake = new StoneIntake();
 
     @Override
     public void init() {
-        mecDrive.initMecanum(hardwareMap);
+        slideDrive.initSlide(hardwareMap);
         stoneIntake.initStoneIntake(hardwareMap);
     }
   
     @Override
     public void loop() {
-        mecDrive.MecanumDrive(-gamepad1.right_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
-        if(gamepad1.right_bumper) {
-            stoneIntake.intakePower(0.5);
-        } else if (gamepad1.left_bumper){
-            stoneIntake.intakePower(-0.5);
-        } else {
-            stoneIntake.intakePower(0);
-        }
+        slideDrive.SlideDrive(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y);
     }
 }
