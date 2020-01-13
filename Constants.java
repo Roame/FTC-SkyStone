5b9a9d1561fe6559b35498095aa562863b324c31
Roame Hildebrand <roamehildebrand@gmail.com>
2020-01-13 10:55:04 -0500

    Competition Corrections.

--- Constants.java
+++ Constants.java
@@ -21,7 +21,7 @@ public final class Constants {
     //For the stone intake:
     public static final String kIntakeRightMotor = "IRM";
     public static final String kIntakeLeftMotor = "ILM";
-    public static  final double kIntakePower = 0.75;
+    public static  final double kIntakePower = 0.5;
 
     //For stone arm system:
     public static final String kArmMotor = "arm motor";
